bf16b8a96b0a3fdf34c162c26f2d8f858119e17f
ChickChicky <chick_chicky@outlook.com>
2024-01-24 20:49:41 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# FAST-TRIP
+
+It was a long way here, just starting from intricate patterns that form when you put sand on top of a metal plate you try to play like a violin with its bow. Somewhere along the line it turned into a weird image generation thing that was slow as hell.
+<br>
+This project was actually really suited for shaders, and it of course outpassed any prior iterations.
+
+# Quick start
+
+* Install [FFMPEG](https://ffmpeg.org/)
+* Install [Node.JS](https://nodejs.org/)
+* Run `npm install`
+* Run `node index.js`
+* Open *renderer.html*
+You should now see the video play on the page and once it stops, it should have finished creating a video file in the same directory.
+
+You can try to edit the shader directly inside the .html file, but I'll try to make a bit more friendly interface for doing so in the future.
---
+++ server.js
@@ -0,0 +1,50 @@
+const ws = require('ws');
+const cp = require('node:child_process')
+
+const server = new ws.Server(
+    {
+        host: 'localhost',
+        port: 4242
+    },
+).on(
+    'connection',
+    ( sock, req ) => {
+        /** @type {cp.ChildProcess} */
+        let proc;
+        let width,height,frames;
+        sock.on('message',(dat,bin)=>{
+            if (bin) {
+                if (dat.slice(0,4).toString('utf-8') == 'INIT') {
+                    width  = dat.readUint32BE(4);
+                    height = dat.readUint32BE(8);
+                    frames = dat.readUint32BE(12);
+                    proc = cp.spawn('ffmpeg',[
+                        '-framerate', '60',
+                        '-threads', '12',
+                        '-s', `${width}x${height}`,
+                        '-f', 'rawvideo',
+                        '-pix_fmt', 'rgb24',
+                        '-i', '-',
+                        '-video_size', `${width}x${height}`,
+                        `${Date.now().toString(36)}.mp4`
+                    ],{
+                        stdio: 'pipe'
+                    });
+                    // proc.stderr.on('data',m=>console.log(m.toString('utf-8')));
+                }
+                else if (dat.slice(0,4).toString('utf-8') == 'DATA') {
+                    proc.stdin.write(dat.slice(4));
+                }
+                else if (dat.slice(0,4).toString('utf-8') == 'END\0') {
+                    proc.stdin.end();
+                }
+            }
+        });
+    }
+).on(
+    'error',
+    console.error
+).on(
+    'listening',
+    () => console.log('ready !')
+);
